71af6b059059a944a32d8118516261cd8c0cf6d4
Attila Szuts <attila.szuts@brokerchooser.com>
2020-11-28 19:30:09 +0100

    finish analysis

--- codes/analysis.R
+++ codes/analysis.R
@@ -1,5 +1,8 @@
-library(tidyverse); library(estimatr); library(scales)
+library(tidyverse); library(estimatr); library(scales); library(lspline); library(texreg); library(car)
 rm(list = ls())
+
+# EDA ---------------------------------------------------------------------
+
 
 # read in data 
 df <- read_csv('data/clean/covid_pop_clean.csv')
@@ -87,11 +90,15 @@ df <- df %>%
          ln_death = log(death),
          ln_cases_sq = ln_cases ^ 2)
 
-# analysis
+
+# analysis ----------------------------------------------------------------
+
+
 # models:
 #   reg1: ln_deaths = alpha + beta * ln_cases
 #   reg2: ln_deaths = alpha + beta_1 * ln_cases + beta_2 * ln_cases_sq
-#   reg3: ln_deaths = alpha + beta * ln_cases , weights : population
+#   reg3: ln_deaths = alpha + beta_1 * ln_cases * 1(cases < ???) + beta_2 * ln_cases * 1(cases >= ???)
+#   reg4: ln_deaths = alpha + beta * ln_cases , weights : population
 
 # reg1 - simple linear regression
 reg1 <- lm_robust(ln_death ~ ln_cases, data = df)
@@ -120,9 +127,25 @@ df %>%
        title = 'COVID-19 cases and deaths') +
   theme_bw() 
 
-# reg3 - weighted OLS
-reg3 <- lm_robust(ln_death ~ ln_cases, data = df, weights = population)
+# reg3 - PLS
+cutoff <- quantile(df$confirmed, .90)
+ln_cutoff <- log(cutoff)
+reg3 <- lm_robust(ln_death ~ lspline(ln_cases, ln_cutoff), data = df)
 summary(reg3)
+
+df %>% 
+  ggplot(aes(ln_cases, ln_death)) +
+  geom_point(color = 'purple', shape = 16, show.legend = F, alpha = 0.6) +
+  geom_smooth(formula = y ~ lspline(x, ln_cutoff), method = "lm", color = 'green') +
+  labs(x = 'Number of confirmed COVID-19 cases, log transformed', 
+       y = 'Number of COVID-19 deaths, log transformed',
+       title = 'COVID-19 cases and deaths') +
+  geom_vline(xintercept = ln_cutoff, linetype = 'dotted') + 
+  theme_bw() 
+
+# reg4 - weighted OLS
+reg4 <- lm_robust(ln_death ~ ln_cases, data = df, weights = population)
+summary(reg4)
 
 df %>% 
   ggplot(aes(ln_cases, ln_death)) + 
@@ -134,14 +157,41 @@ df %>%
        title = 'COVID-19 cases and deaths') +
   theme_bw() 
 
-reg4 <- lm_robust(death ~ ln_cases + ln_cases_sq, data = df)
-summary(reg4)
 
-df %>% 
-  ggplot(aes(ln_cases, death)) + 
-  geom_point(color = 'purple', shape = 16, show.legend = F, alpha = 0.6) +
-  geom_smooth(method = "lm", formula = y ~ poly(x, 2), color = 'green') +
-  labs(x = 'Number of confirmed COVID-19 cases, log transformed', 
-       y = 'Number of COVID-19 deaths, log transformed',
-       title = 'COVID-19 cases and deaths') +
-  theme_bw() 
+
+# create model comparison table -----------------------------------------------------------
+
+data_out <- 'out/'
+htmlreg(list(reg1 , reg2 , reg3 , reg4),
+        type = 'html',
+        custom.model.names = c("Simple linear","Quadratic linear","PLS", "Weighted OLS"),
+        caption = "Modelling case fatality and confirmed COVID-19 cases",
+        file = paste0( data_out ,'model_comparison.html'), 
+        include.ci = FALSE)
+
+## based on these results, I chose simple linear regression, because even though it has a smaller R^2 than 
+## the weighted OLS model, it's SE is half, and the R^2 difference is not that big.
+
+# hypothesis testing ------------------------------------------------------
+
+linearHypothesis( reg1 , "ln_cases = 0")
+
+# residual analysis -------------------------------------------------------
+
+# Get the predicted y values from the model
+df$reg1_y_pred <- reg1$fitted.values
+# Calculate the errors of the model
+df$reg1_res <- df$ln_death - df$reg1_y_pred 
+# calculate the predicted number of deaths (inverse log transformation)
+df$reg1_y_pred_exp <- exp(df$reg1_y_pred)
+
+# Find countries with largest negative errors
+df %>% top_n( -5 , reg1_res ) %>% 
+  select( country , confirmed, death , reg1_y_pred_exp, reg1_y_pred , ln_death , reg1_res ) %>% 
+  arrange(reg1_res)
+
+# Find countries with largest positive errors
+df %>% top_n( 5 , reg1_res ) %>% 
+  select( country , confirmed, death , reg1_y_pred_exp, reg1_y_pred , ln_death , reg1_res ) %>% 
+  arrange(desc(reg1_res))
+
